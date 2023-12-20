c1ee8350575cd1bc5bfabdc4c86a32a66bee8646
Tom Bartram <t.bartram@hotmail.co.uk>
2023-12-20 10:23:50 +0000

    fix: error handling for connection termination

--- src/models/EPOSMessage.java
+++ src/models/EPOSMessage.java
@@ -5,4 +5,11 @@ public class EPOSMessage {
   public String type;
   public String currency;
   public String value;
+
+  public EPOSMessage(Integer id, String type, String currency, String value) {
+    this.id = id;
+    this.type = type;
+    this.currency = currency;
+    this.value = value;
+  }
 }
